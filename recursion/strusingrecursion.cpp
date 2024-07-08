fa2005f0840a20c8f5c8c47c889caea4c6971e2c
virat <singhbirat2@gmail.com>
2024-07-08 17:04:14 +0530

    str

--- recursion/strusingrecursion.cpp
+++ recursion/strusingrecursion.cpp
@@ -1,28 +1,23 @@
 #include<iostream>
 using namespace std ;
 void   reverseusingrecursion(int s ,int e , string& arr ){
-      if(e<s) {
+      if(e<s) 
         return;
-      }
+      
       else{
       swap(arr[s],arr[e]);
       s++;
       e--;
         reverseusingrecursion(s,e,arr);
       }
-
-
 }
-
 int main(){
-  string arr = "virat";
+  string arr = "tribhuwan";
   int n=arr.length()-1;
     reverseusingrecursion(0,n,arr);
     for (int i = 0; i <= n; i++)
     {
       cout<<arr[i];
     }
-    
-
   return 0 ;
 }
